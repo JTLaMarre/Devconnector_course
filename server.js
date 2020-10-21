2f664fcfbd90e3cb39eb0234d31338cc83c4d3a0
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-10-21 09:31:45 -0400

    routes defined

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ connectDB();
 // sends browser that api is working
 app.get('/', (req,res)=>res.send(`API running`))
 
+// define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 // if no enviroment variable sends to port 5000
 const PORT = process.env.PORT || 5000;
 
